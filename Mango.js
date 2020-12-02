1f8ce5df8c98258c07d9d58d057375dd1096eecc
sohancoder308 <sohanpatil2109@gmail.com>
2020-12-02 11:38:31 +0530

    Mango Class

---
+++ Mango.js
@@ -0,0 +1,26 @@
+class Mango {
+    constructor (x,y,r){
+        var options = {
+          isStatic : true,
+          restitution : 0,
+          friction : 1.0    
+        }
+      this.x = x;
+      this.y = y;
+      this.r = r;
+      this.image = loadImage("Images/mango.png");
+      this.body = Bodies.circle(this.x,this.y,(this.r)/2,options);
+      World.add(world,this.body);  
+    }
+   display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x,pos.y);
+      angleMode(RADIANS);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image,0,0,this.r,this.r);
+      pop();
+   } 
+}
